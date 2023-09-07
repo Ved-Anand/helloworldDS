014d0a5f749ebb441be7efc6af57a63233ffca51
Ved Anand <ved.anand@outlook.com>
2023-09-06 18:29:44 -0700

    added parameter take in functionality

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,15 @@
 public class HelloWorld {
 
-public static void main(String[] args) {
-	System.out.println("Hello World");
-}
+	public static void main(String[] args) {
+		if (args.length == 0) {
+			System.out.println("Hello World");
+		} else if (args[0].equals("hello")) {
+			System.out.println("Hello World");
+		} else if (args[0].equals("goodbye")) {
+			System.out.println("Goodbye, cruel world!");
+		} else {
+			System.out.println("Hello World"); // input isnt hello or goodbye
+		}
+	}
 
 }
